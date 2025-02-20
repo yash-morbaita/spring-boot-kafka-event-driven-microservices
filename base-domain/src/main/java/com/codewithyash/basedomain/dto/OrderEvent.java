2a6f9d81b4175c9952e8edb5bfc1152720ffb89d
Yash Morbita <yashmorbaita@gmail.com>
2025-02-19 22:09:03 -0800

    CODEWITHYASH initial coding, need to solve some bugs

---
+++ base-domain/src/main/java/com/codewithyash/basedomain/dto/OrderEvent.java
@@ -0,0 +1,12 @@
+package com.codewithyash.basedomain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data @NoArgsConstructor @AllArgsConstructor
+public class OrderEvent {
+    private String message;
+    private String status;
+    private Order order;
+}
